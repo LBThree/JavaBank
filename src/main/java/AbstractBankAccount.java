c87028f60ae290e05f2a121c89cf9bfb53dd3e68
LBThree <loganbahr94@gmail.com>
2020-10-28 18:04:15 -0400

    starting point for the JavaBank project

---
+++ src/main/java/AbstractBankAccount.java
@@ -0,0 +1,12 @@
+public abstract class AbstractBankAccount {
+
+  public final String BANK = "JavaBank";
+
+  public abstract void deposit(int amt);
+
+  public abstract void withdraw(int amt);
+
+  public abstract int getBalance();
+
+  public abstract String getBankName();
+}
